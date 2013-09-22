22c1b299f244acd6cc643d4c8cd41e5c9103f856
Yanno12 <yann-o@hotmail.fr>
2013-09-22 21:12:47 +0000

    modified web.js

--- web.js
+++ web.js
@@ -1,12 +1,8 @@
-var express = require("express");
-var app = express();
-app.use(express.logger());
+var http = require("http");
+var fs = require("fs");
+var index = fs.readFileSync('index.html');
 
-app.get("/", function(request, response) {
-    response.send("Yanno le rigolo !!");
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+http.createServer(function (req, res) {
+  res.writeHead(200, {"Content-Type": "text/html"});
+  res.end(index);
+}).listen(9615);
