c9af4668dd6ab58c655e77fdd49c316ece094a88
Yanno12 <yann-o@hotmail.fr>
2013-09-24 10:21:55 +0000

    added new web.js

--- web.js
+++ web.js
@@ -1,9 +1,16 @@
-var express = require("express");
-var app = express();
-app.use(express.logger());
+var express = require('express');
+var fs      = require('fs');
 
-app.get("/", function(request, response) {
-var data = fs.readFileSync("index.html").toString();response.send(data);
+
+var getTextFromFile = function(filename){
+    return fs.readFileSync(filename).toString();
+};
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  var source_file = "index.html";
+  response.send(getTextFromFile(source_file));
 });
 
 var port = process.env.PORT || 5000;
