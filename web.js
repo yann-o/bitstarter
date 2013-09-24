bedac679b01bd6c73f708670705128864da07dee
Yanno12 <yann-o@hotmail.fr>
2013-09-24 10:14:32 +0000

    changed web.js

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.logger());
 
 app.get("/", function(request, response) {
-    response.send("Yanno le rigolo !!");
+var data = fs.readFileSync("index.html").toString();response.send(data);
 });
 
 var port = process.env.PORT || 5000;
